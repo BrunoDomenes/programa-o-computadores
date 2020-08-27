4406c7305f857f4a7d8ef1d87e1d19a9170e346b
unknown <brunodomenes134@gmail.com>
2020-08-26 21:56:31 -0300

    exe9 calculando normalmente

--- list1 - exe9.js
+++ list1 - exe9.js
@@ -1,1 +1,20 @@
-console.log(`exercicio 9`)
+//EXERCICIO 9
+chefe()
+
+function chefe(){
+
+    var numero = Number(prompt(`Informe um numero`))
+
+    exe9(numero)
+}
+
+function exe9(num){
+
+    var soma = 0
+    for(var divisor=num; divisor >=1; divisor--){ //divisor começa em 6 e vai até 1
+        if (num % divisor == 0){
+            soma = soma + divisor
+        }
+    }
+    console.log(`A soma dos divisores de ${num} é ${soma}`)
+}
